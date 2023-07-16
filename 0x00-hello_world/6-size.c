acdf4cfb816be641233dbad056db6caa86564217
Mayar128 <mayar.nageeb@gmail.com>
2023-07-16 23:38:27 +0300

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry of the code
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+    puts("Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 8 byte(s)\nSize of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)\n");
+    return (0);
+}
