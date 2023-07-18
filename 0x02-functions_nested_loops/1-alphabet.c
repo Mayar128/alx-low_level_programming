5360825db647d57d2388212ff2552a161f076e81
Mayar128 <mayar.nageeb@gmail.com>
2023-07-19 02:14:14 +0300

    task 0

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,17 @@
 #include "main.h"
+
+/**
+ * print_alphabet - prints the lower cas alphbetics
+ *
+ * Return - this function return nothing
+*/
 void print_alphabet(void)
 {
 	char alpha = 'A';
 
 	while (alpha >= 97 && alpha <= 122)
 	{
-		puts(alpha);
+		putchar(alpha);
 		alpha++;
 	}
 }
